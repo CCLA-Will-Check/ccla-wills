e9fd4e67459456bebbc6c2fe0423c00181daa1dc
Sam Popowich <spopowich@gmail.com>
2017-03-25 12:45:10 -0600

    Add request validation.

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,7 @@
 class Request < ApplicationRecord
+  validates name, presence: true
+  validates testator_first, presence: true
+  validates testator_last, presence: true
+  validates testator_dob, presence: true
+  validates testator_city, presence: true
 end
