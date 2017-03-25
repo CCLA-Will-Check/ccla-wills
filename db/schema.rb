cd9e9007a65a8d93b66fe735c8efc895c0f6af75
Sam Popowich <spopowich@gmail.com>
2017-03-25 14:46:28 -0600

    Switch from sqlite to postgres.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170325184312) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "firms", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
