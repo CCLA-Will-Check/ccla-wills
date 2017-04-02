7871181bd9fef2733e05ad451b1e10bb65304908
Sam Popowich <spopowich@gmail.com>
2017-04-02 17:21:48 -0600

    Close #22.

--- app/models/will.rb
+++ app/models/will.rb
@@ -2,16 +2,16 @@ class Will < ApplicationRecord
   validates_presence_of :first_name, :last_name, :dob, :city_of_birth, :country_of_birth, :lawyer_name, :firm_name, :lsuc, :telephone
 
   def self.search(search)
-    where("first_name LIKE :search \
-          or last_name LIKE :search \
-          or alt_last_name LIKE :search \
-          or city_of_birth LIKE :search \
-          or province_of_birth LIKE :search \
-          or country_of_birth LIKE :search \
-          or last_resided LIKE :search \
-          or lawyer_name LIKE :search \
-          or firm_name LIKE :search \
-          or telephone LIKE :search", {search: "%#{search}%"})
+    where("first_name ILIKE :search \
+          or last_name ILIKE :search \
+          or alt_last_name ILIKE :search \
+          or city_of_birth ILIKE :search \
+          or province_of_birth ILIKE :search \
+          or country_of_birth ILIKE :search \
+          or last_resided ILIKE :search \
+          or lawyer_name ILIKE :search \
+          or firm_name ILIKE :search \
+          or telephone ILIKE :search", {search: "%#{search}%"})
   end
 
   def self.import(file)
