4bac451295eb19f47f8cc2db0442ca7b4003b3d0
Sam Popowich <spopowich@gmail.com>
2017-06-17 09:32:07 -0600

    Make some fields non-mandatory.

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,7 +2,5 @@ class Request < ApplicationRecord
   validates :applicant, presence: true
   validates :testator_first, presence: true
   validates :testator_last, presence: true
-  validates :testator_dob, presence: true
-  validates :testator_city, presence: true
   validates :status, presence: true
 end
