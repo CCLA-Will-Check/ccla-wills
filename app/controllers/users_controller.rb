6a38cc4f445b56453e4a3f216daf39fbd085a1e2
Sam Popowich <spopowich@gmail.com>
2017-07-22 09:44:17 -0600

    #54 #56

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
 
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
   def show
     @user = User.find(params[:id])
